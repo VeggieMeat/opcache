fb2bb46ffe78ec29fc51b02e3e7bbd250ffe8ee5
Brian Altenhofel <brian.altenhofel@vmdoh.com>
2015-05-02 14:24:03 -0500

    use Crunch/FastCGI instead

--- includes/OPCache/FCGIRequest.php
+++ includes/OPCache/FCGIRequest.php
@@ -2,7 +2,6 @@
 
 namespace OPCache;
 
-use mikehaertl\shellcommand\Command;
 
 class FCGIRequest {
 
@@ -14,28 +13,21 @@ class FCGIRequest {
 
   public function run() {
     global $base_url;
-    $this->command = new Command(
+    $fastcgi = new \Crunch\FastCGI\Client($this->fcgi);
+    $connection = $fastcgi->connect();
+    $request = $connection->newRequest(
       array(
-        'command' => 'cgi-fcgi -bind -connect ' . $this->fcgi,
-        'procEnv' => array(
-          'SCRIPT_NAME' => '/index.php',
-          'SCRIPT_FILENAME' => DRUPAL_ROOT . '/index.php',
-          'HTTP_HOST' => $base_url,
-          'REMOTE_ADDR' => '127.0.0.1',
-          'QUERY_STRING' => $this->queryString,
-          'REQUEST_METHOD' => 'GET',
-          'SERVER_SOFTWARE' => 'Drupal',
-          'REQUEST_URI' => $this->uri,
-        ),
+        'GATEWAY_INTERFACE' => 'FastCGI/1.0',
+        'REQUEST_METHOD' => 'GET',
+        'SERVER_SOFTWARE' => 'Drupal',
+        'SCRIPT_NAME' => '/index.php',
+        'SCRIPT_FILENAME' => DRUPAL_ROOT . '/index.php',
+        'QUERY_STRING' => $this->queryString,
+        'REQUEST_URI' => $this->uri,
       )
     );
 
-    if ($this->command->execute()) {
-      return substr($this->command->getOutput(), 8, 3);
-    }
-    else {
-      return $this->command->getExitCode();
-    }
+    $response = $connection->request($request);
   }
 
 }

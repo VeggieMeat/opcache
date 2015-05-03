4ec621e46aeb170308c25ffeec971f2367fcb536
Brian Altenhofel <brian.altenhofel@vmdoh.com>
2015-05-02 21:24:32 -0500

    fix missed static override on HTTPRequest

--- includes/OPCache/HTTPRequest.php
+++ includes/OPCache/HTTPRequest.php
@@ -5,7 +5,7 @@ namespace OPCache;
 class HTTPRequest extends \GuzzleHttp\Client {
   const VERSION = '7.x-1.x';
 
-  public function getDefaultUserAgent() {
+  public static function getDefaultUserAgent() {
     return 'OPCache/' . self::VERSION . ' ' . parent::getDefaultUserAgent();
   }
 }
